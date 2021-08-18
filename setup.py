517ab79672b170e9289488fe0294fe2e256afcf0
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-08-19 00:10:20 +0300

    📄 update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
     description = ""
 
 setup(name='clrfterm',
-      version='0.3',
+      version='1.0.0',
       description="Module that decorates your console",
       packages=['clrfterm'],
       long_description=description,
